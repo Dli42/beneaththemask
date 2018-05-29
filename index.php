ebdb1d4010f7222b75fe955d2c8b92d70cf84982
David Li <davelid136@gmail.com>
2018-05-29 18:25:03 -0400

    moving pictures around and fixing autoplay

--- index.php
+++ index.php
@@ -5,42 +5,48 @@
 <head>
 	<style>
 		@font-face { font-family: p5; src: url('p5hatty.ttf'); } 
-		table {
+		table, h1 {
 			font-family: p5;
 			font-size: 30;
+			color: #EEEEEE;
 		}
 		th {
 			padding-right: 50px;
 		}
+		body {
+			background-color: black;
+		}
 	</style>
 </head>
 
-<audio id="audio-default" autoplay loop>
+<audio id="audio-default" controls autoplay loop>
 	<source id="source-default" src="music/beneath_the_mask.mp3" type="audio/mp3"></source>
 </audio>
 
-<audio id="audio-default-instr" autoplay loop>
+<audio id="audio-default-instr" controls autoplay loop>
 	<source id="source-default-instr" src="music/beneath_the_mask_instr.mp3" type="audio/mp3"></source>
 </audio>
 
-<audio id="audio-rain" autoplay loop>
+<audio id="audio-rain" controls autoplay loop>
 	<source id="source-rain" src="music/beneath_the_mask_rain.mp3" type="audio/mp3"></source>
 </audio>
 
-<audio id="audio-rain-instr" autoplay loop>
+<audio id="audio-rain-instr" controls autoplay loop>
 	<source id="source-rain-vocals" src="music/beneath_the_mask_rain_instr.mp3" type="audio/mp3"></source>
 </audio>
 
 <div>
 	<table>
-		<th id="instr" onclick="switchTrack('audio-default')">Default</th>
-		<th id="vocals" onclick="switchTrack('audio-default-instr')">Instr</th>
-		<th id="rain-instr" onclick="switchTrack('audio-rain')">Rain</th>
-		<th id="rain-vocals" onclick="switchTrack('audio-rain-instr')">Rain Intr</th>
+		<th id="instr" onclick="switchTrack('audio-default')" onmouseover="" style="cursor: pointer;">Default</th>
+		<th id="vocals" onclick="switchTrack('audio-default-instr')" onmouseover="" style="cursor: pointer;">Instr</th>
+		<th id="rain-instr" onclick="switchTrack('audio-rain')" onmouseover="" style="cursor: pointer;">Rain</th>
+		<th id="rain-vocals" onclick="switchTrack('audio-rain-instr')" onmouseover="" style="cursor: pointer;">Rain Intr</th>
+		<th id="mute" onclick="switchTrack('mute')" onmouseover="" style="cursor: pointer;">Mute</th>
+		<th id="play" onclick="play()" onmouseover="" style="cursor: pointer;" hidden>Play</th>
 	</table>
 </div>
 
-<h1 id="demo"></h1>
+<h1 id="demo">test</h1>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
 	var currentTrack = "audio-default";
@@ -48,6 +54,26 @@
 	document.getElementById('audio-default-instr').volume = 0.0;
 	document.getElementById('audio-rain').volume = 0.0;
 	document.getElementById('audio-rain-instr').volume = 0.0;
+
+	var promise = document.querySelector('audio').play();
+
+	if (promise !== undefined) {
+	  promise.then(_ => {
+	    // Autoplay started!
+	  }).catch(error => {
+	    // Autoplay was prevented.
+	    // Show a "Play" button so that user can start playback.
+	    document.getElementById('play').hidden = false;
+	  });
+	}
+
+	function play(){
+		document.getElementById('audio-default').play();
+		document.getElementById('audio-default-instr').play();
+		document.getElementById('audio-rain').play();
+		document.getElementById('audio-rain-instr').play();
+		document.getElementById('play').hidden = true;
+	}
 	
 	function switchTrack(trackName){
 
